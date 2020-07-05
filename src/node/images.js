caee1302851597f2a44205462389e47e55423d50
Matt York <my0373@gmail.com>
2020-07-05 13:46:41 +0100

    Refactored the fullpath methos for the image object.

--- src/node/images.js
+++ src/node/images.js
@@ -2,19 +2,16 @@ let image = {
     // A simple object to describe an image.
     filename: 'undefined',
     path: 'undefined',
-    fullpath: 'undefined',
+    fullpath: function ()
+    {
+        // Just a test to print the output of an imaginary object and set it's property
+        return `${this.path}/${this.filename}`;
+    },
     file_size: file_size = 0
 }
 
-let getFullImagePath = function (image)
-{
-    // Just a test to print the output of an imaginary object and set it's property.
-    image.fullpath = `${image.path}/${image.filename}`;
-    return image.fullpath
-
-}
 
 // log the objects full path.
-console.log(getFullImagePath(image))
+console.log(image.fullpath())
 
 
